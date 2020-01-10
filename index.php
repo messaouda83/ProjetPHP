b884f514ef37705c05b6c1610b4a2343c8e20107
messaouda83 <messaoudabenchikh@yahoo.fr>
2020-01-10 10:32:23 +0100

    mon premier commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+<!-- Debut de header -->
+<header class="head">
+
+</header>
+<!-- Fin header -->
+<!-- Début de Contenu -->
+<div class="container"></div>
+<!-- Fin Contenu -->
+
+<!-- Début footer -->
+<footer>
+
+<p>© 2020 Messaouda</p>
+</footer>
+<!-- Fin footer -->
+
+ <?php
+ ?>   
+</body>
+</html>
