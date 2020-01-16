52d19a1618e1786d0a046a456336b89bca8aa45b
messaouda83 <messaoudabenchikh@yahoo.fr>
2020-01-16 09:34:43 +0100

    projet final

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 </head>
 <body>
         <!-- Début header -->
-<header class="nav-menu">
-	<label for="nav-toggle" class="nav-toggle-label"></label>
-</header>
+  <header class="nav-menu">
+    <label for="nav-toggle" class="nav-toggle-label"></label>
+  </header><!--Fin Header -->
 <!-- Début Menu Burgger -->
 <input type="checkbox" id="nav-toggle" class="nav-toggle">				
 <!-- Début Menu navigation -->
@@ -22,25 +22,23 @@
 <li><a href="tester.php">Tester</a></li>
 </ul>
 <!-- Main page -->
-  <div id="post-body">
-  <h1><span>Projet en PHP</span> Utiliser uniquement CSS et HTML</h1>
-    
-    <p><b>L'objectif de ce projet</b> et de   créer un site avec du PHP qui permet aux utilisateurs de réviser leurs tables de multiplication.
-    Une page index permettra de se rendre sur 2 pages différentes : une pour voir les tables et une pour tester ses connaissances.</p> 
+    <div id="post-body">
+    <h1><span>Projet en PHP</span> Utiliser uniquement CSS et HTML</h1>
+      
+      <p><b>L'objectif de ce projet</b> et de   créer un site avec du PHP qui permet aux utilisateurs de réviser leurs tables de multiplication.
+      Une page index permettra de se rendre sur 2 pages différentes : une pour voir les tables et une pour tester ses connaissances.</p> 
 
-    <p>La page pour voir les tables aura un formulaire qui permettra de sélectionner une ou plusieurs tables de multiplication (exemple : celle du 6, du 8, du 9), après soumission de celui-ci : les tables sélectionner apparaîtront en dessous de la page.</p>
+      <p>La page pour voir les tables aura un formulaire qui permettra de sélectionner une ou plusieurs tables de multiplication (exemple : celle du 6, du 8, du 9), après soumission de celui-ci : les tables sélectionner apparaîtront en dessous de la page.</p>
 
-    <p>La page pour tester ses connaissances aura un formulaire pour sélectionner une seule table cette fait. Une fois cette table sélectionnée vous ferez apparaître un nouveau formulaire (condition PHP pour générer du HTML à combiner avec la méthode du formulaire que vous désirez). Ce formulaire proposera un calcul à faire au hasard (ex : 6 fois 3 = ?) et aura un input dans lequel on pourra rentrer sa réponse. Une fois cette réponse soumise, on affichera si la réponse est bonne ou non et le formulaire proposera un nouveau calcul à faire (ex : 6 fois 8 = ?), etc…
-  .</p>
+      <p>La page pour tester ses connaissances aura un formulaire pour sélectionner une seule table cette fait. Une fois cette table sélectionnée vous ferez apparaître un nouveau formulaire (condition PHP pour générer du HTML à combiner avec la méthode du formulaire que vous désirez). Ce formulaire proposera un calcul à faire au hasard (ex : 6 fois 3 = ?) et aura un input dans lequel on pourra rentrer sa réponse. Une fois cette réponse soumise, on affichera si la réponse est bonne ou non et le formulaire proposera un nouveau calcul à faire (ex : 6 fois 8 = ?), etc…
+    .</p>
 
-    <p>Objectif : Utiliser du PHP pour gérer vos pages (pour ne pas avoir à réécrire notre header et notre footer sur chaque page), utiliser les superglobales PHP : $_ et pourquoi pas découvrir une nouvelle superglobale qui pourrait vous aider pour garder des données le temps de visite du site (session), algorithmie, faire un beau site.</p>
-  </div>
+      <p>Objectif : Utiliser du PHP pour gérer vos pages (pour ne pas avoir à réécrire notre header et notre footer sur chaque page), utiliser les superglobales PHP : $_ et pourquoi pas découvrir une nouvelle superglobale qui pourrait vous aider pour garder des données le temps de visite du site (session), algorithmie, faire un beau site.</p>
+    </div>
 
-  <footer> <!-- Début footer -->
-
-  <p>Formation ASC 2019- 2020</p>
-
-  </footer> <!-- Fin footer -->
+    <footer> <!-- Début footer -->
+    <p>Formation ASC 2019- 2020</p>
+    </footer> <!-- Fin footer -->
         
 
 </body>
