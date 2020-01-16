61feaa1fa542b76ac7f726fb72dc3f5cc23982bb
messaouda83 <messaoudabenchikh@yahoo.fr>
2020-01-16 09:00:42 +0100

    finition

--- reviser.php
+++ reviser.php
@@ -1,97 +1,85 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css" />
-    <title>Mon premier projet PHP</title>
-    
-</head>
-<body>
-<header class="nav-menu">
-	<label for="nav-toggle" class="nav-toggle-label"></label>
-</header>
 
-<input type="checkbox" id="nav-toggle" class="nav-toggle">				
+    <head>
 
-<ul id="menu-topo">
-<ul>
-<li><a href="index.php">Accueil</a></li>
-<li><a href="reviser.php">Réviser</a></li>
-<li><a href="tester.php">Tester</a></li>
-
-
-</ul>
-</ul>
-
-<div id="post-body">
-<h1><span>Réviser</span> les tables de multiplications</h1>
- <br>
-<form action="reviser.php" method="post">
-
-<?php
-
-for($i=1; $i<=10; $i++){
-    echo"<input type=\"checkbox\" name=\"choix[]\" value=\"$i\">$i ";
-}
-?>
-<br>
-<br>
-<br>
-
-<input type="submit" value="Afficher tables de multiplications" >
-
-
-</form>
- 
-<?php
-
-
-if (isset($_POST['choix'])) {
-
-    var_dump($_POST['choix']);
-
-
-
-    foreach ($_POST['choix'] as $key => $value){
-           
-
-      
-     for($i=1; $i<=10; $i++)
-     {
-         $result = $value*$i;
-         echo'<br>';
-         echo"$value*$i = $result<br>";
-     
-     }
-
-     echo"<br>";
-   
-    }
-    
-}
-
-/*  
-$tab1=[1,2,3,4,5,6,7,8,9,10];
-for($i=1;$i<=10; $i++){
-    echo"<tr>";
-    for($j=1;$j<=10; $j++){
-     echo("<td>".$i*$j."</td>");
-    }
-    echo"</tr>";
-
-}
-*/
-?></p>
-</div>
- <!-- Début footer -->
-<div class="footer">
-  <p>Formation ASC 2019- 2020</p>
-</div>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="style.css?<?php echo"". time() .""; ?>" />
+        <title>Mon premier projet PHP</title>
         
- <!-- Fin footer -->
- <?php
- ?>   
-</body>
+    </head>
+
+    <body>
+
+        <header class="nav-menu">
+            <label for="nav-toggle" class="nav-toggle-label"></label>
+        </header>
+
+        <input type="checkbox" id="nav-toggle" class="nav-toggle">				
+
+        <ul id="menu-topo">
+
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="reviser.php">Réviser</a></li>
+            <li><a href="tester.php">Tester</a></li>
+
+        </ul>
+
+        <div id="post-body">
+
+            <h1><span>Réviser</span> les tables de multiplications</h1><br>
+
+            <form action="reviser.php" method="post">
+
+                <?php
+
+                    for($i=1; $i<=10; $i++){
+
+                        if (isset($_POST['choix']) && in_array($i, $_POST['choix'])) $checked = "checked"; else $checked = ""; 
+
+                        echo"<input type=\"checkbox\" name=\"choix[]\" value=\"$i\"  $checked >$i ";
+                    }
+
+                ?>
+
+                <br><br><br>
+
+                <input type="submit" value="Afficher tables de multiplications" >
+
+            </form>
+            
+            <?php
+
+                if (isset($_POST['choix'])) 
+                {
+                    foreach ($_POST['choix'] as $key => $value)
+                    {
+                        for($i=1; $i<=10; $i++)
+                        {
+                            $result = $value*$i;
+                            echo'<br>';
+                            
+                            echo"$value*$i = $result<br>";
+                            
+
+                        }
+
+                        echo"<br>";
+                    } 
+                }
+
+            ?>
+
+        </div>
+
+        <footer> <!-- Début footer -->
+
+            <p>Formation ASC 2019- 2020</p>
+
+        </footer> <!-- Fin footer -->
+
+    </body>
+
 </html>
--- tester.php
+++ tester.php
@@ -4,88 +4,95 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css" />
-    <title>Mon premier projet PHP</title>
-    
-</head>
-<body>
-<header class="nav-menu">
-	<label for="nav-toggle" class="nav-toggle-label"></label>
-</header>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="style.css?<?php echo"". time() .""; ?>" />
+        <title>Mon premier projet PHP</title>
+        
+    </head>
 
-<input type="checkbox" id="nav-toggle" class="nav-toggle">				
+    <body>
 
-<ul id="menu-topo">
-<ul>
-<li><a href="index.php"><i class="icon-home"></i>Accueil</a></li>
-<li><a href="reviser.php">Réviser</a></li>
-<li><a href="tester.php">Tester</a></li>
+        <header class="nav-menu">
+            <label for="nav-toggle" class="nav-toggle-label"></label>
+        </header>
 
+        <div id="nav-toggle" class="nav-toggle"></div>				
 
-</ul>
-</ul>
+        <ul id="menu-topo">
+            <li><a href="index.php"><i class="icon-home"></i>Accueil</a></li>
+            <li><a href="reviser.php">Réviser</a></li>
+            <li><a href="tester.php">Tester</a></li>
+        </ul>
 
-<div id="post-body">
-<h1><span>Tester </span>vos connaissances</h1>
- <br>
-<form action="tester.php" method="get">
+        <div id="post-body">
 
-<p>Sélectionner une table</p>	
-<select name="select" id ="select" >
-    <?php	
-  
-    $_GET['nbr']=$_SESSION['form']['nbr'];
-    for ($i=1;$i<=10;$i++)	
-        {
-        if ($i==$_GET['nbr']) echo '<option value="'.$i.'" selected="selected">'.$i.'</option>';
-        else echo '<option value="'.$i.'">'.$i.'</option>';
-        }
-      
+            <h1><span>Tester </span>vos connaissances</h1><br>
 
-    ?>
-</select>
-<input name="Envoyer" type="submit" value="confirmer" >
-</form>
-<form action="tester.php" method="post" >
-<?php
- 
-if(isset($_GET['select'])){
-   $random=rand(1,10);
-    $select = $_GET['select'];
-    $result=$random*$select;
-    echo $select."*".$random."="."?";
-    echo "<br>";
-    echo "<input type=\"number\" name=\"valeur\" value=\"\" >"; 
-    echo"<br>";
-    echo "<input name=\"Envoyer\" type=\"submit\"  onclick=\"test();>\">";
-    
-}
-    ?> 
-</form>
-<?php
-function test(){
-  
-    if(isset($_POST['valeur'])){
-        //int pour convertir en nombre
-    if((int)$_POST['valeur']===$result){
+            <form action="tester.php" method="get">
 
-      echo"bravo!";
-    }
-   } 
-}
-?>
+                <select name="select" id ="select" onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
 
+                    <?php	
 
-</p>
-</div>
- <!-- Début footer -->
-<div class="footer">
-  <p>Formation ASC 2019- 2020</p>
-</div>     
- <!-- Fin footer -->
-</body>
+                        if(isset($_GET['select'])) $matable = $_GET['select']; else $matable = "";
+
+                        if (!$matable) echo"<option value=\"\">Selectionnez une table</option>";
+
+                        for ($i=1;$i<=10;$i++)	
+                        {
+                            if ($i== $matable) echo '<option value="'.$i.'" selected>'.$i.'</option>';
+                            else echo '<option value="tester.php?select='.$i.'">'.$i.'</option>';
+                        }
+                    ?>
+                    
+                </select>
+
+            </form>
+
+            <form action="" method="get" >
+
+                <?php
+            
+                    if(isset($_GET['select'])) $select = $_GET['select']; else $select = "";
+                    if(isset($_GET['random'])) $random = $_GET['random']; else $random = "";
+                    if(isset($_GET['valeur'])) $valeur = $_GET['valeur']; else $valeur = "";
+
+                    // Comparaison du résultat et de la bonne réponse
+                    if($select && $random && $valeur)
+                    {
+                        $result =  $select * $random ;
+                        //echo"La reponse : $random * $select = $result ( repondu $valeur )---<br><br>";
+                        echo"La reponse : $select * $random = $result<br>";
+                        if($result == $valeur) echo"<br>bravo!<br><br>"; else echo"perdu<br><br>";
+                    }
+
+                    if($select)
+                    {
+                        $random=rand(1,10);
+                        $result=$random*$select;
+
+                        echo $select."*".$random."="."?<br>";
+
+                        echo "<input type=\"number\" name=\"valeur\" value=\"\" >";
+                        echo "<input type=\"hidden\" name=\"select\" value=\"$matable\" >";
+                        echo "<input type=\"hidden\" name=\"random\" value=\"$random\" >";
+                        echo"<br><br><input name=\"Envoyer\" type=\"submit\" value=\"Confirmer\" >"; 
+                    }
+                    
+                ?> 
+            </form>
+
+        </div>
+
+        <footer> <!-- Début footer -->
+
+            <p>Formation ASC 2019- 2020</p>
+
+        </footer> <!-- Fin footer -->
+
+    </body>
+
 </html>
